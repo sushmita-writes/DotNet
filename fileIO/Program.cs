7040790734a51eaa7a6e56f4875fc74f42892a40
Sushmita Shrestha <shresthasush3225@gmail.com>
2026-08-02 18:15:41 +0545

    new proj

---
+++ fileIO/Program.cs
@@ -0,0 +1,15 @@
+﻿FileStream F = new FileStream("test.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+for (int i = 0; i < 10; i++)
+{
+    F.WriteByte((byte)i);
+}
+
+Console.WriteLine("Data in test.dat:");
+F.Position = 0;
+for (int i = 0; i < 10; i++)
+{
+    Console.Write(F.ReadByte());
+}
+F.Close();
+Console.ReadKey();
+
