d60c29d5afad4ad62e10240d639755ae9d51be25
Sushmita Shrestha <sushmita.shrestha.713@gmail.com>
2026-08-16 15:46:04 +0545

    Portfolio Complete

---
+++ Portfolio/Controllers/PortfolioController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("my-introduction")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [Route("few-of-my-ongoing-or-completed-projects")]
+        public IActionResult Projects()
+        {
+            return View();
+        }
+    }
+}
