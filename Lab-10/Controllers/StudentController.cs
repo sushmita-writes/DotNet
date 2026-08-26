079eb2793a74ed019cf6d8efba2137ac1a9ba932
Sushmita Shrestha <sushmita.shrestha.713@gmail.com>
2026-08-26 17:45:41 +0545

    commit

---
+++ Lab-10/Controllers/StudentController.cs
@@ -0,0 +1,37 @@
+﻿using Lab_10.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab_10.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Detail()
+        {
+            StudentModel student = new StudentModel();
+            student.Age = 21;
+            student.FirstName = "Sushmita";
+            student.LastName = "Shrestha";
+            student.Grade = 'A';
+            student.PhoneNumber = 9800000000;
+            return View(student);
+        }
+
+        public IActionResult Details()
+        {
+            List<StudentModel> students = new List<StudentModel>
+            {
+                new StudentModel {Age = 21, FirstName = "Sushma", LastName = "Rai", Grade = 'A', PhoneNumber = 9711111111},
+                new StudentModel {Age = 23,FirstName = "Ram", LastName = "Karki", Grade = 'C', PhoneNumber = 97222222222},
+                new StudentModel {Age = 25, FirstName = "Shyam", LastName = "Khadka", Grade = 'F', PhoneNumber =  9733333333},
+                new StudentModel {Age = 20, FirstName = "Sita", LastName = "Tamang", Grade = 'B', PhoneNumber = 9811111111 }
+            };
+
+            return View(students);
+        }
+    }
+}
---
+++ Lab-10/Models/StudentModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab_10.Models
+{
+    public class StudentModel
+    {
+        public int Age { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public char Grade { get; set; }
+        public long PhoneNumber {  get; set; }
+
+    }
+}
